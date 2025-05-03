96f384256e7dcb602fdb311441afae393fa7a994
Teddy Bradford <3684553+teddybradford@users.noreply.github.com>
2025-05-03 08:56:03 -0400

    Simplify repo organization

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@bearform/css/tonic.css'
+import '../../tonic/index.css'
 import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
